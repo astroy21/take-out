b5103e538d46a875c0dd22a1d5c2708e5f3daa75
yeyuyuan <211220155@smail.nju.edu.cn>
2023-12-23 22:06:37 +0800

    update: shop status

---
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -0,0 +1,28 @@
+package com.sky.controller.user;
+
+import com.sky.result.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+//为2个shopcontroller指定不同的名字防止bean冲突
+@RestController("userShopController")
+@RequestMapping("/user/shop")
+@Slf4j
+public class ShopController {
+    public static final String KEY = "SHOP_STATUS";
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 查询店铺状态
+     * @return
+     */
+    @GetMapping("/status")
+    public Result<Integer> getStatus(){
+        Integer status = (Integer) redisTemplate.opsForValue().get(KEY);
+        log.info("查询店铺状态为{}",status);
+        return Result.success(status);
+    }
+}
