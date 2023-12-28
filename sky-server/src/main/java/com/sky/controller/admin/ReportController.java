e93ab04979e9dd61be236d4df2548d9f2bf567a9
yeyuyuan <211220155@smail.nju.edu.cn>
2023-12-28 15:41:31 +0800

    update: top 10 statistics

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
@@ -51,6 +52,12 @@ public class ReportController {
         return Result.success(userReportVO);
     }
 
+    /**
+     * 订单统计
+     * @param begin
+     * @param end
+     * @return
+     */
     @GetMapping("/ordersStatistics")
     public Result<OrderReportVO> ordersStatistics(
             @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate begin,
@@ -60,4 +67,20 @@ public class ReportController {
         OrderReportVO orderReportVO = reportService.getOrderStatistics(begin,end);
         return Result.success(orderReportVO);
     }
+    /**
+     * 销量top10
+     * @param begin
+     * @param end
+     * @return
+     */
+    @GetMapping("/top10")
+    public Result<SalesTop10ReportVO> top10(
+            @DateTimeFormat(pattern = "yyyy-MM-dd")LocalDate begin,
+            @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end
+    ){
+        log.info("销量前10:{}-{}",begin,end);
+        SalesTop10ReportVO salesTop10ReportVO = reportService.getSalesTop10(begin,end);
+        return Result.success(salesTop10ReportVO);
+    }
+
 }
